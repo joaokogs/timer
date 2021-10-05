1430335d7a8d98b621f8f865a58adfeaca723fd2
koohzin <joao.koguishi@gmail.com>
2021-10-04 22:53:37 -0300

    bugs

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ var intervalo;
 
 function iniciar(){
     intervalo = setInterval(timer,10)
+    document.querySelector('.start').style.display = 'none';
 }
+
+
+
 function pausar(){
     clearInterval(intervalo)
+    document.querySelector('.start').style.display = '';
 }
 function parar(){
     clearInterval(intervalo)
@@ -15,17 +20,18 @@ function parar(){
     sec=0
     min=0
     document.querySelector('#tempo').innerText='00:00.00'
+    document.querySelector('.start').style.display = '';
 }
 function timer(){
-    mili++
-    if(mili==100){
-        sec++
-        mili=0
-        if(sec==60){
-            min++
-            sec=0
+        mili++
+        if(mili==100){
+            sec++
             mili=0
-        }
-    }
+            if(sec==60){
+                min++
+                sec=0
+                mili=0
+            }
+        } 
     document.querySelector('#tempo').innerText=min+':'+sec+'.'+mili
 }
